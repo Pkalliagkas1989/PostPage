79feaa905dcb260de45ed7338b7bf3919124b874
George Patoulas <patou505@gmail.com>
2025-06-05 18:37:03 +0300

    add function to fetch categories and populate create post modal

--- ui/static/js/modal.js
+++ ui/static/js/modal.js
@@ -27,4 +27,26 @@ document.addEventListener("DOMContentLoaded", () => {
     postBody.value = "";
     modal.classList.add("hidden");
   };
+
+  async function populateCategories() {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/forum/api/categories"
+      );
+      const data = await response.json();
+
+      const select = document.getElementById("post-category");
+
+      data.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.name.toLowerCase().replace(/\s+/g, "-");
+        option.textContent = category.name;
+        select.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  }
+
+  populateCategories();
 });
