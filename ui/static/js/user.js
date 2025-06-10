0eee375742c0ff97cbef8e12a1b0a852b8b5430c
George Patoulas <patou505@gmail.com>
2025-06-10 18:26:59 +0300

    fix reactions to be able to take back like or dislike

--- ui/static/js/user.js
+++ ui/static/js/user.js
@@ -1,4 +1,5 @@
 function countReactions(reactions = []) {
+  if (!Array.isArray(reactions)) reactions = [];
   return {
     likes: reactions.filter((r) => r.reaction_type === 1).length,
     dislikes: reactions.filter((r) => r.reaction_type === 2).length,
