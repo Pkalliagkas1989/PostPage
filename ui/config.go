bb5afd344af390927e380f74907b0d63857282bb
mkouvara <marinoskouvaras@gmail.com>
2025-06-13 15:29:14 +0300

    docker

--- ui/config.go
+++ ui/config.go
@@ -1,13 +1,13 @@
-package main
+ package main
 
-var (
-	APIBaseURL    = "http://localhost:8080/forum/api"
-	AuthURI       = APIBaseURL + "/session/verify"
-	DataURI       = APIBaseURL + "/guest"
-	LoginURI      = APIBaseURL + "/session/login"
-	LogoutURI     = APIBaseURL + "/session/logout"
-	RegisterURI   = APIBaseURL + "/register"
-	CategoriesURI = APIBaseURL + "/categories"
-	ReactionsURI  = APIBaseURL + "/react"
-	CommentsURI   = APIBaseURL + "/comments"
-)
+// var (
+// 	APIBaseURL    = "http://localhost:8080/forum/api"
+// 	AuthURI       = APIBaseURL + "/session/verify"
+// 	DataURI       = APIBaseURL + "/guest"
+// 	LoginURI      = APIBaseURL + "/session/login"
+// 	LogoutURI     = APIBaseURL + "/session/logout"
+// 	RegisterURI   = APIBaseURL + "/register"
+// 	CategoriesURI = APIBaseURL + "/categories"
+// 	ReactionsURI  = APIBaseURL + "/react"
+// 	CommentsURI   = APIBaseURL + "/comments"
+// )
