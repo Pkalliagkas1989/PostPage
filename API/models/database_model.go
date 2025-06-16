ad7d30a7c642e2d269800c612279a2d465a38b4f
mkouvara <marinoskouvaras@gmail.com>
2025-06-16 11:56:26 +0300

    minor change on the  index

--- API/models/database_model.go
+++ API/models/database_model.go
@@ -77,14 +77,14 @@ func createTables(db *sql.DB) error {
 
 	// Define all table creation SQL statements
 	tableStatements := []string{
-		config.IdxPostsUserID,
-		config.IdxPostCategoriesPostID,
-		config.IdxPostCategoriesCategoryID,
-		config.IdxCommentsPostID,
-		config.IdxCommentsUserID,
-		config.IdxReactionsUserID,
-		config.IdxReactionsPostID,
-		config.IdxReactionsCommentID,
+		config.CreateUserTable,
+		config.CreateUserAuthTable,
+		config.CreateSessionsTable,
+		config.CreateCategoriesTable,
+		config.CreatePostsTable,
+		config.CreateCommentsTable,
+		config.CreateReactionsTable,
+		config.CreatePostCategoriesTable,
 	}
 
 	// Execute each table creation statement
@@ -109,14 +109,14 @@ func createIndexes(db *sql.DB) error {
 
 	// Define all index creation SQL statements
 	indexStatements := []string{
-		config.CreateUserTable,
-		config.CreateUserAuthTable,
-		config.CreateSessionsTable,
-		config.CreateCategoriesTable,
-		config.CreatePostsTable,
-		config.CreateCommentsTable,
-		config.CreateReactionsTable,
-		config.CreatePostCategoriesTable,
+		config.IdxPostsUserID,
+		config.IdxPostCategoriesPostID,
+		config.IdxPostCategoriesCategoryID,
+		config.IdxCommentsPostID,
+		config.IdxCommentsUserID,
+		config.IdxReactionsUserID,
+		config.IdxReactionsPostID,
+		config.IdxReactionsCommentID,
 	}
 
 	// Execute each index creation statement
